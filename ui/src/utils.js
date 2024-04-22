ae1610fbb293a9ed7cf14d7068715750ed526f7c
Anuj Tiwari <135423006+anujtiwari18@users.noreply.github.com>
2024-04-22 15:00:04 +0530

    Update utils.js

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -1,1 +1,1 @@
-export const API_URL = "https://s4dfc5ebyhzobihpunp63tfi4e0rqgwj.lambda-url.ap-south-1.on.aws/task";
+export const API_URL = "https://s4dfc5ebyhzobihpunp63tfi4e0rqgwj.lambda-url.ap-south-1.on.aws/tasks";
